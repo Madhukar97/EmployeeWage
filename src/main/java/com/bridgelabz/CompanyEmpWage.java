548d57d903ddc0f391fa1c6c7bde01da0098a778
unknown <madhukar.tatiraju@gmail.com>
2021-10-28 00:41:39 +0530

    UC11 added

---
+++ src/main/java/com/bridgelabz/CompanyEmpWage.java
@@ -0,0 +1,68 @@
+package com.bridgelabz;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CompanyEmpWage implements WageBuilder {
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
+    int wagePerHr;
+    int empHrs = 0;
+    int monthlyWage = 0;
+    int totalWorkingHrs;
+    int totalWorkingDays;
+    int workedHrs = 0;
+    static int i;
+    static int company1TotalWage;
+    static int company2TotalWage;
+
+    public static void main(String[] args) {
+
+        CompanyEmpWage mainObj = new CompanyEmpWage();
+        mainObj.compEmpWageArray();
+
+        WageBuilder interfaceObj = new CompanyEmpWage();
+        interfaceObj.empWageBuilder();
+    }
+    public void compEmpWageArray() {
+        CompanyEmpWage company1 = new CompanyEmpWage();
+        CompanyEmpWage company2 = new CompanyEmpWage();
+
+        company1TotalWage = company1.empWageBuilder();
+        System.out.println("Employee wage for company1: " + company1TotalWage);
+        company2TotalWage = company2.empWageBuilder();
+        System.out.println("Employee wage for company2: " + company2TotalWage);
+
+        CompanyEmpWage[] companyEmpWageArray = new CompanyEmpWage[2];
+        companyEmpWageArray[0] = company1;
+        companyEmpWageArray[1] = company2;
+        System.out.println(Arrays.toString(companyEmpWageArray));
+    }
+    public int empWageBuilder() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the wage_per_hr: ");
+        wagePerHr = scanner.nextInt();
+        System.out.println("Enter the total Working days: ");
+        totalWorkingDays = scanner.nextInt();
+        System.out.println("Enter the total working hrs in a month: ");
+        totalWorkingHrs = scanner.nextInt();
+
+        for (i = 0; i < totalWorkingDays && workedHrs < totalWorkingHrs; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME -> empHrs = 8;
+                case IS_PART_TIME -> empHrs = 4;
+            }
+            int empWage = empHrs * wagePerHr;
+            System.out.println("Employee wage is: " + empWage);
+            monthlyWage += empWage;
+            workedHrs += empHrs;
+        }
+        System.out.println("Employee Monthly wage is: " + monthlyWage);
+        System.out.println("Working Days = " + i + ", Total working hrs = " + workedHrs);
+        return monthlyWage;
+    }
+}
+interface WageBuilder {
+    int empWageBuilder();
+}
